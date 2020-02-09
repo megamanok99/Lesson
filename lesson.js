69188038b6e70af22bad4838eabb3f32a35866ef
Sanya <megamanok99@gmail.com>
2020-02-09 21:30:56 +0300

    first lesson

--- lesson.js
+++ lesson.js
@@ -1,1 +1,20 @@
-alert("Hello world");
+let money= prompt("Ваш бюджет на месяц?");
+let time= prompt("Введите дату в формате YYYY-MM-DD");
+let appData={
+    money: money,
+    time : time,
+    expenpenses:{
+        
+    },
+    optionalExpenses:{},
+    income : [],
+    savings:false
+};
+let answer1= prompt("Введите обязательную статью расходов в этом месяце");
+let answer2= prompt("Во сколько обойдется?");
+
+appData.expenpenses.answer1=answer2;
+for (let key in appData){
+    console.log(key+":"+appData[key]);
+}
+alert(appData.money/30);
