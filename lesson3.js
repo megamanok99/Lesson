99bc82367ab23978d170b8daa1ac86ff11825e33
Sanya <megamanok99@gmail.com>
2020-02-10 19:39:07 +0300

    lesson 2

---
+++ lesson3.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+let num=40;
+/*while(i<50){
+    i++;
+    console.log(i);
+}
+do{
+    console.log(i);
+    i++;
+
+}
+while(i<45);
+for(let i=1;i<8;i++){
+    if(i==6){
+        continue;
+    }
+  console.log(i) ; 
+}*/
+let x = 4,
+    y= 5,
+    x1=2,
+    x2=1;
+if(x==(x1-3) && y==(x2-1)) {
+    console.log("true");
+}
+else {
+    console.log(" nit true");
+}
