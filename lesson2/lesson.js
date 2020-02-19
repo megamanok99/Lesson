80c4352ca6a9445b4f06b0d1ba85031d34e878f5
Sanya <megamanok99@gmail.com>
2020-02-19 21:15:02 +0300

    4 lesson

--- lesson2/lesson.js
+++ lesson2/lesson.js
@@ -1,80 +1,43 @@
 /*jshint esversion: 6 */
-let money,time;
-function start(){
-     money = +prompt("Ваш бюджет на месяц?","");
-     time= prompt("Введите дату в формате YYYY-MM-DD");
-    while(isNaN(money) || money=="" || money==null) {
-        money = +prompt("Ваш бюджет на месяц?",""); 
-    }
+/*let arr=['firsrt',2,'third',4,5];
+//arr.pop();
+//arr.push('5');
+//arr.shift();
+//arr.unshift('1');
+//for(let i=0;i<arr.length;i++){
+//    console.log(arr[i]);
+//}
+arr.forEach(function(item,i,mass){
+    console.log(i+':' + item+mass);
+
+});
+console.log(arr);
+
+let mass=[1,3,4,5,6];
+for(let key  of mass){
+    console.log(key);
+}*/
+//let ans=prompt('','');
+//arr=[];
+//arr= ans.split(',');
+//console.log(arr);
+//let arr=['awae','zzz','sew','wew'];
+//let i= arr.join(', ');
+//console.log(i);
+let arr=[1,25,4];
+let i= arr.sort(compareNum);
+function compareNum(a,b){
+    return a-b;
 }
-start();
+console.log(i);
 
-let appData={
-    money: money,
-    time : time,
-    expenses:{},
-    optionalExpenses:{},
-    income : [],
-    savings:true
-};
-function chooseExpenses(){
-
-    for(i=0;i<2;i++){
-        let a= prompt("Введите обязательную статью расходов в этом месяце"),
-            b= prompt("Во сколько обойдется?");
-        
-        if((typeof(a))==='string' && (typeof(a))!= null && (typeof(b))!= null &&
-        a !="" && b!="" &&  a.length<50  ){
-            console.log('done');
-            appData.expenses[a] =b;
-        }
-        else{
-            alert("вы ошиблись с заполнением,обновите страницу");
-        }
-    
-    }
+let soldier={
+    healrth:400,
+    armor:100
 }
-chooseExpenses();
-function detectDayBudget(){
-    appData.moneyPerDay=(appData.money/30).toFixed();
-alert("ежедневный бюджет: "+appData.moneyPerDay);
+let john={
+    health:100
 }
-detectDayBudget();
-
-function detectLevel(){
-    if(appData.moneyPerDay<100){
-        console.log("minimalka");
-    }
-    else if(appData.moneyPerDay<2000){
-        console.log("medium");
-    }
-    else if(appData.moneyPerDay>2000){
-        console.log("high");
-    }
-    else{ 
-            console.log("oshibka");
-    }
-}
-detectLevel();
-
-function checkSavings(){
-    if (appData.savings==true){
-        let save=+prompt("какова сумма накоплений?"),
-            persent=+prompt("под какой процент?");
-    
-            appData.monthIncome=save/100/12*persent;
-    }
-    
-    alert("доход в месяц с вашего депозита ="+ appData.monthIncome);
-}
-
-checkSavings();
-
-function chooseOptExpenses(){
-    for(i=1;i<4;i++){
-    appData.optionalExpenses[i]=prompt("статья необязательных расходов");
-    console.log('done');
-
-    }
-}
-chooseOptExpenses();
+john.__proto__=soldier;
+console.log(john);
+console.log(john.armor);
